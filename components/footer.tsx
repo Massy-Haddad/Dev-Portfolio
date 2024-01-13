551af4b78742e457f5cc19e7b1d660e48bae63c1
Massy Haddad <massy.haddad@lepisc.com>
2024-01-13 17:37:28 -0500

    Changes : header navigation progress

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<footer className="mb-10 px-4 text-center text-gray-500">
 			<small className="mb-2 block text-xs">
-				&copy;2024 MASSY. ALL RIGHT RESERVED.
+				&copy; 2024 MASSY. ALL RIGHT RESERVED.
 			</small>
 			<p className="text-xs">
 				<span className="font-semibold">À propos de ce Portfolio:</span> Réalisé
--- components/header.tsx
+++ components/header.tsx
@@ -4,10 +4,13 @@ import Image from 'next/image'
 import clsx from 'clsx'
 import { motion } from 'framer-motion'
 
-import Logo from '@/public/assets/logo.png'
+
 import { routes } from '@/lib/data'
 import { useScrollTop } from '@/hooks/use-scroll-top'
 import { useActiveSectionContext } from '@/context/active-section-context'
+
+import Logo from '@/public/assets/logo.png'
+import { FaLanguage } from 'react-icons/fa'
 
 export default function Header() {
 	const scrolled = useScrollTop()
@@ -21,19 +24,19 @@ export default function Header() {
 			animate={{ y: 0, opacity: 1 }}
 			exit={{ y: -10, opacity: 0 }}
 			transition={{ duration: 0.5, ease: 'easeOut' }}
-			className={
-				'sticky top-4 left-0 right-0 z-10 mt-8 max-w-5xl mx-auto w-full'
-			}
+			className={clsx(
+				'sticky top-0 left-0 right-0 z-10 mx-auto w-full backdrop-blur-[8px] bg-opacity-10 border-b border-transparent transition-all duration-500',
+				{
+					'border-slate-900 dark:border-slate-900 bg-gray-50': scrolled,
+				}
+			)}
 		>
-			<nav className="flex justify-center">
+			<nav className="flex justify-center py-4">
 				<div className="relative">
 					<ul
-						className={clsx(
-							'inline-flex items-center max-w-full h-12 m-auto rounded-lg backdrop-filter backdrop-blur-sm bg-opacity-10 text-gray-500',
-							{
-								'bg-[#1f2023cc]': scrolled,
-							}
-						)}
+						className={
+							'inline-flex items-center w-full h-12 m-auto text-[0.9rem] text-gray-500 p-0'
+						}
 					>
 						<div className="flex items-center justify-between w-full h-full gap-2 capitalize pt-3 pr-2 pb-3 pl-3">
 							<Link
@@ -47,29 +50,37 @@ export default function Header() {
 									height={150}
 									className="block w-100 h-auto dark:brightness-200"
 								/> */}
-								<span className="text-3xl dark:text-slate-50">MASSY</span>
+								<span className="block text-3xl dark:text-slate-50">MASSY</span>
 							</Link>
 
-							{routes.map((link) => (
-								<li className="h-3/4 flex items-center justify-center relative">
-									<Link
-										className={clsx(
-											'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
-											{
-												'text-gray-950 dark:text-gray-200':
-													activeSection === link.title,
-											}
-										)}
-										href={link.href}
-										onClick={() => {
-											setActiveSection(link.title)
-											setTimeOfLastClick(Date.now())
-										}}
-									>
-										{link.title}
-									</Link>
-								</li>
-							))}
+							<div className="flex-shrink">
+								<div className="flex items-center justify-between w-full h-full gap-2 capitalize py-3 pr-2 pl-3">
+									{routes.map((link) => (
+										<li key={link.title} className="">
+											<Link
+												className={clsx(
+													'flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300',
+													{
+														'text-gray-950 dark:text-gray-200':
+															activeSection === link.title,
+													}
+												)}
+												href={link.href}
+												onClick={() => {
+													setActiveSection(link.title)
+													setTimeOfLastClick(Date.now())
+												}}
+											>
+												{link.title}
+											</Link>
+										</li>
+									))}
+								</div>
+							</div>
+
+							<span className="text-3xl">
+								<FaLanguage />
+							</span>
 						</div>
 					</ul>
 				</div>
